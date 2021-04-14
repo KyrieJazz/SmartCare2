23396be8c5fd2b4c03af1ac1ebc0c6d06ca389cf
kyriejazz <657435299@qq.com>
2021-04-14 15:17:40 +0800

    4.13 Monitor页面完成播放器配置 bug：点击全屏按钮之后，不仅视频没有全屏，且应用整体变成横屏模式，无法恢复. 4.14 添加组件显示当前时间

--- app/src/main/java/com/example/smartcare2/ui/home/MonitorFragment.java
+++ app/src/main/java/com/example/smartcare2/ui/home/MonitorFragment.java
@@ -8,20 +8,27 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.dueeeke.videoplayer.player.VideoView;
 import com.dueeeke.videocontroller.StandardVideoController;
 
 import com.example.smartcare2.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MonitorFragment extends Fragment {
 
     private MonitorViewModel mViewModel;
 
     private VideoView videoView;
+
+    private TextView time;
 
     public static MonitorFragment newInstance() {
         return new MonitorFragment();
@@ -33,7 +40,10 @@ public class MonitorFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_monitor, container, false);
         //初始化视频播放器
         init_videoView(root);
-
+        //显示当前时刻
+//        time = (TextView) root.findViewById(R.id.curr_time);
+//        Log.d("setCurr_time返回值", setCurr_time());
+//        time.setText("这里出现问题");
         return root;
     }
 
@@ -41,14 +51,22 @@ public class MonitorFragment extends Fragment {
     //初始化播放器
     protected void init_videoView(View view) {
         videoView = (VideoView) view.findViewById(R.id.player);
-        videoView.setUrl("rtmp://58.200.131.2:1935/livetv/hunantv");
+        videoView.setUrl("http://ivi.bupt.edu.cn/hls/cctv5phd.m3u8");
         //设置播放器的控制器
         StandardVideoController controller = new StandardVideoController(getContext());
-        controller.addDefaultControlComponent("RTMP测试——湖南电视台直播", false);
+        controller.addDefaultControlComponent("直播测试", false);
         //将该控制器与播放器关联
         videoView.setVideoController(controller);
         videoView.start();
     }
+
+    //显示当前时刻
+    protected String setCurr_time() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        Date date = new Date(System.currentTimeMillis());
+        return simpleDateFormat.format(date);
+    }
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
